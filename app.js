9f2c82087ae261b83b9cbb26dfb5044e8a9313e8
andreyrise <andrey.rise@gmail.com>
2022-12-24 18:41:50 -0800

    make it better

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 5000;
+const port = procces.env.PORT || 5000;
 
 app.use(bodyParser.json());
 
@@ -11,24 +11,24 @@ app.use(bodyParser.json());
 const uri = "mongodb+srv://LittleLemon:LittleLemon@cluster0.xt8ni2b.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 client.connect(err => {
-  const collection = client.db("littlelemon").collection("bookings");
+    const collection = client.db("littlelemon").collection("bookings");
 
-  // Set up API endpoint.
-  app.post('/bookings', (req, res) => {
-    // Validate request data.
-    if (!req.body.date || !req.body.time || !req.body.people) {
-      return res.status(400).send({ error: 'Invalid request data' });
-    }
-    // Insert booking data into database.
-    collection.insertOne({ date: req.body.date, time: req.body.time, people: req.body.people }, (error, result) => {
-      if (error) {
-        return res.status(500).send({ error: 'Error inserting booking data' });
-      }
-      res.send({ message: 'Booking created successfully' });
+    // Set up API endpoint.
+    app.post('/bookings', (req, res) => {
+        // Validate request data.
+        if (!req.body.date || !req.body.time || !req.body.people) {
+            return res.status(400).send({ error: 'Invalid request data' });
+        }
+        // Insert booking data into database.
+        collection.insertOne({ date: req.body.date, time: req.body.time, people: req.body.people }, (error, result) => {
+            if (error) {
+                return res.status(500).send({ error: 'Error inserting booking data' });
+            }
+            res.send({ message: 'Booking created successfully' });
+        });
     });
-  });
 
-  app.listen(port, () => {
-    console.log(`Booking API listening on port ${port}`);
-  });
+    app.listen(port, () => {
+        console.log(`Booking API listening on port ${port}`);
+    });
 });
